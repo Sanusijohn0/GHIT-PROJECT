f5c57610b3f49adc6dc5cfe8048b3d3dac66edc5
sanusi john <sanusijohn0@gmail.com>
2024-12-25 12:38:50 +0100

    finished home page and starting courses page

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,31 @@
-const nav = document.getElementById("nav");
 const icon = document.getElementById("navbar-toggler");
+const backToTop = document.querySelector(".backtotop");
+const loginForm = document.getElementById("login-form");
+const signUpForm = document.getElementById("signup-form");
+const loginButton = document.getElementById("login-button");
+const cancelButton = document.getElementById("cancel");
+const cancelButtonTwos = document.getElementById("cancel2");
 
 icon.onclick = () => {
-  nav.classList.toggle("mobile-nav");
   icon.classList.toggle("hover");
-  nav.classList.add("fixed");
+  const navItem = document.querySelector(".navbar-nav");
+  navItem.classList.toggle("show");
 };
+
+document.addEventListener("scroll", () => {
+  backToTop.style.display = "flex";
+});
+
+loginButton.addEventListener("click", () => {
+  loginForm.style.display = "flex";
+});
+cancelButton.addEventListener("click", () => {
+  loginForm.style.display = "none";
+});
+window.addEventListener("load", () => {
+  signUpForm.style.display = "flex";
+});
+
+cancelButtonTwos.addEventListener("click", () => {
+  signUpForm.style.display = "none";
+});
